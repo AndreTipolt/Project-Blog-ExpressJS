3e0bc34737dea76b06e3dd4a24e52607fd397f93
AndreTipolt <andretipoltlopes@gmail.com>
2023-03-10 11:17:42 -0300

    Added Search Input

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,61 +1,55 @@
 import { Request, Response } from "express";
 import { postRepository } from "../repositories/PostRepository";
 import { date, z } from 'zod'
+import { Like } from 'typeorm'
 
 export class PostController {
+
     static async getCreate(req: Request, res: Response) {
         return res.render('createPost')
     }
+
     static async home(req: Request, res: Response) {
 
         const currentDate = new Date()
+
+        const search = req.query.search as string
+
+        if (search) {
+
+            const postSearch = await (await postRepository
+                .find({ where: { content: Like(`%${search}%`) }, relations: { user: true } }))
+                .map((post) => {
+
+                    post.user.password = ''
+                    const diff = currentDate.getTime() - post.date.getTime()
+                    const baseDiffHour = diff / 1000 / 60 / 60
+
+                    const diffDates = controllerDate(baseDiffHour)
+
+                    return { post, diffDates }
+                })
+
+            return res.status(200).render('home', { posts: postSearch })
+        }
+
         const posts = (await postRepository.find({ relations: { user: true } }))
             .map((post) => {
 
                 post.user.password = ''
 
                 const diff = currentDate.getTime() - post.date.getTime()
-                const baseDiffHour = diff / 1000 / 60 / 60 
+                const baseDiffHour = diff / 1000 / 60 / 60
 
-                let diffDates;
+                const diffDates = controllerDate(baseDiffHour)
 
-                if(baseDiffHour < 1){ // Minutes
-
-                    const minutes = baseDiffHour * 60
-
-                    if(minutes < 1){
-
-                        diffDates = 'Agora Mesmo'
-
-                    } else{
-
-                        diffDates = `há ${minutes.toFixed(0)} Minutos`
-                    }
-
-                }else if(baseDiffHour > 24){ // Days
-
-                    const days = baseDiffHour / 24
-
-                    if(days >= 7){ // Weeks
-
-                        const weeks = days / 7
-                        diffDates = `há ${weeks.toFixed(0)} Semanas`
-
-                    } else{
-
-                        diffDates = `há ${days.toFixed(0)} Dias`
-                    }
-
-                }
-                else{ // Hours
-                    diffDates = `há ${baseDiffHour.toFixed(0)} Horas`
-                }
-                
                 return { post, diffDates }
+
             })
 
         return res.status(200).render('home', { posts })
     }
+
 
     static async create(req: Request, res: Response) {
 
@@ -87,4 +81,41 @@ export class PostController {
         }
 
     }
+
+}
+
+function controllerDate(baseDiffHour: number) {
+    let diffDates
+    if (baseDiffHour < 1) { // Minutes
+
+        const minutes = baseDiffHour * 60
+
+        if (minutes < 1) {
+
+            diffDates = 'Agora Mesmo'
+
+        } else {
+
+            diffDates = `há ${minutes.toFixed(0)} Minutos`
+        }
+
+    } else if (baseDiffHour > 24) { // Days
+
+        const days = baseDiffHour / 24
+
+        if (days >= 7) { // Weeks
+
+            const weeks = days / 7
+            diffDates = `há ${weeks.toFixed(0)} Semanas`
+
+        } else {
+
+            diffDates = `há ${days.toFixed(0)} Dias`
+        }
+
+    }
+    else { // Hours
+        diffDates = `há ${baseDiffHour.toFixed(0)} Horas`
+    }
+    return diffDates
 }
